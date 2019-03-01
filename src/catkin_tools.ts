f67c06a80f44d77cda3e9acff73d9b4c6c04714b
buck <sebastian.buck@uni-tuebingen.de>
2019-03-01 20:32:06 +0100

    Add package.xml dependency completion

--- src/catkin_tools.ts
+++ src/catkin_tools.ts
@@ -416,6 +416,13 @@ export async function registerProviders(context: vscode.ExtensionContext) {
           'Catkin tools only supports C/C++ API 2.0 or later.');
     }
   }
+
+
+  const package_xml_provider = vscode.languages.registerCompletionItemProvider(
+      {pattern: '**/package.xml'},
+      new CatkinPackageCompleterXml(catkin_workspace));
+
+  context.subscriptions.push(package_xml_provider);
 }
 export function reloadCompileCommand() {
   status_bar_item.text = status_bar_prefix + 'reloading';
@@ -423,4 +430,51 @@ export function reloadCompileCommand() {
   provider.loadDataBases();
 
   status_bar_item.text = status_bar_prefix + 'reload complete';
+}
+
+
+
+export class CatkinPackageCompleterXml implements
+    vscode.CompletionItemProvider {
+  private workspace: CatkinWorkspace;
+
+  constructor(workspace: CatkinWorkspace) {
+    this.workspace = workspace;
+  }
+
+
+  provideCompletionItems(
+      document: vscode.TextDocument, position: vscode.Position,
+      token: vscode.CancellationToken, context: vscode.CompletionContext) {
+    let lines = document.getText().split('\n');
+
+    let ctx = lines[position.line].slice(0, position.character).trim();
+
+    let snippets = [];
+
+    if (ctx.match('<[^/]*depend>')) {
+      for (var pkg of this.workspace.packages) {
+        let item = new vscode.CompletionItem(pkg.name);
+        item.documentation = 'Add dependency: ' + pkg.name;
+        item.command = {
+          title: 'Close Tag',
+          command: 'closeTag.closeHTMLTag'
+        };
+        snippets.push(item);
+      }
+    } else {
+      for(var type of ["depend", "build_depend", "build_export_depend", "exec_depend"]) {
+        let item = new vscode.CompletionItem(`<${type}>`);
+        item.range = document.getWordRangeAtPosition(position, /[^\s]+/);
+        item.kind = vscode.CompletionItemKind.Keyword;
+        item.command = {
+          title: 'Suggest',
+          command: 'editor.action.triggerSuggest'
+        };
+        snippets.push(item);
+      }
+    }
+
+    return new vscode.CompletionList(snippets, true);
+  }
 }
