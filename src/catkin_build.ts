35c8723a3d4bb5605e64f7d7bd4c0677fe565340
Sebastian Buck <mail@betwo.eu>
2019-05-05 10:52:56 +0200

    Make catkin build task work without open editor

--- src/catkin_build.ts
+++ src/catkin_build.ts
@@ -15,32 +15,37 @@ export async function getCatkinBuildTask(): Promise<vscode.Task[]> {
     return emptyTasks;
   }
 
+  let source_catkin = 'catkin locate > /dev/null && source "$(catkin locate -d)/setup.bash"';
+  let source_workspace = 'cd "${workspaceFolder}" && ' + source_catkin;
+  let source_current_package = 'cd "${fileDirname}" && ' + source_catkin;
+
   let result: vscode.Task[] = [];
   {
     let taskName = 'build';
-    let kind: CatkinTaskDefinition = {type: 'catkin_build', task: taskName};
+    let kind: CatkinTaskDefinition = { type: 'catkin_build', task: taskName };
     let task = new vscode.Task(
-        kind, taskName, 'catkin_build',
-        new vscode.ShellExecution('cd ${fileDirname} && source $(catkin locate -d)/setup.bash && catkin build'),
-        ['$catkin-gcc', '$catkin-cmake']);
+      kind, taskName, 'catkin_build',
+      new vscode.ShellExecution(source_workspace + ' && catkin build'),
+      ['$catkin-gcc', '$catkin-cmake']);
     result.push(task);
   }
   {
     let taskName = 'build current package';
-    let kind: CatkinTaskDefinition = {type: 'catkin_build', task: taskName};
+    let kind: CatkinTaskDefinition = { type: 'catkin_build', task: taskName };
     let task = new vscode.Task(
-        kind, taskName, 'catkin_build',
-        new vscode.ShellExecution('cd ${fileDirname} && source $(catkin locate -d)/setup.bash && catkin build --this -v --no-deps'),
-            ['$catkin-gcc', '$catkin-cmake']);
+      kind, taskName, 'catkin_build',
+      new vscode.ShellExecution(source_current_package + ' && catkin build --this -v --no-deps'),
+      ['$catkin-gcc', '$catkin-cmake']);
     result.push(task);
   }
   {
     let taskName = 'run current package tests';
-    let kind: CatkinTaskDefinition = {type: 'catkin_build', task: taskName};
+    let kind: CatkinTaskDefinition = { type: 'catkin_build', task: taskName };
     let task = new vscode.Task(
-        kind, taskName, 'catkin_build',
-        new vscode.ShellExecution('cd ${fileDirname} && source $(catkin locate -d)/setup.bash && env CTEST_OUTPUT_ON_FAILURE=1 catkin build --this -v --no-deps --make-args test'),
-            ['$catkin-gcc', '$catkin-cmake']);
+      kind, taskName, 'catkin_build',
+      new vscode.ShellExecution(source_current_package + ' && ' +
+      'env CTEST_OUTPUT_ON_FAILURE=1 catkin build --this -v --no-deps --make-args test'),
+      ['$catkin-gcc', '$catkin-cmake']);
     result.push(task);
   }
   return result;
