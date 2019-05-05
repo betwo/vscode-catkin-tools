d9c68ef899efc18c80e0d69713455c9fa30aadb6
Sebastian Buck <mail@betwo.eu>
2019-05-05 10:52:26 +0200

    Remove unused/old/wrong package exports

--- src/catkin_build.ts
+++ src/catkin_build.ts
@@ -5,11 +5,6 @@ interface CatkinTaskDefinition extends vscode.TaskDefinition {
    * The task name
    */
   task: string;
-
-  /**
-   * The rake file containing the task
-   */
-  file?: string;
 }
 
 
