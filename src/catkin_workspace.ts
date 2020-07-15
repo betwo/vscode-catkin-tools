bc36191f2fbe4866b229a8b9039a65bd8f5af507
Sebastian Buck <mail@betwo.eu>
2020-07-15 15:41:11 +0200

    Remove bashism

--- src/catkin_workspace.ts
+++ src/catkin_workspace.ts
@@ -497,9 +497,10 @@ export class CatkinWorkspace {
     command += `pushd . > /dev/null; cd "${vscode.workspace.rootPath}";`;
     command += `${payload}`;
     if (!payload.endsWith(";")) {
-      command += "; EXIT_CODE=$?; ";
+      command += "; ";
     }
-    command += `popd > /dev/null; [ $EXIT_CODE -eq 0 ] || exit $EXIT_CODE;`;
+    command += "EXIT_CODE=$?; ";
+    command += `popd > /dev/null; [ "$EXIT_CODE" = "0" ] || exit $EXIT_CODE;`;
     return command;
   }
 }
