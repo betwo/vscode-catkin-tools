5c4c4ccb8c381225d19b64a508a5951c78f31b73
Sebastian Buck <mail@betwo.eu>
2022-02-15 01:29:15 +0100

    Add optional callbacks to runCommand for access to stdout

--- src/catkin_tools/catkin_command.ts
+++ src/catkin_tools/catkin_command.ts
@@ -1,11 +1,19 @@
 import * as fs from 'fs';
+import * as child_process from 'child_process';
 
 import { runCommand, ShellOutput } from '../common/shell_command';
 
 
-export async function runCatkinCommand(args: string[], cwd: fs.PathLike): Promise<ShellOutput> {
+export async function runCatkinCommand(
+    args: string[],
+    cwd: fs.PathLike,
+    additional_env_vars?: [string, string][],
+    callback?: (process: child_process.ChildProcess) => any,
+    out?: (lines: string) => void,
+    error?: (lines: string) => void
+): Promise<ShellOutput> {
     try {
-        return await runCommand("catkin", args, [], cwd);
+        return await runCommand("catkin", args, [], cwd, additional_env_vars, callback, out, error);
     } catch (error) {
         console.error(error);
         throw error;
--- src/common/shell_command.ts
+++ src/common/shell_command.ts
@@ -1,4 +1,5 @@
 import * as child_process from 'child_process';
+import { assert } from 'console';
 import * as fs from 'fs';
 import { getExtensionConfiguration } from './configuration';
 
@@ -12,21 +13,32 @@ export class ShellOutput {
     }
 }
 
+export function getShell(): string {
+    return getExtensionConfiguration('shell');
+}
+
+export function getShellExtension(): string {
+    return getExtensionConfiguration('shell');
+}
+
+export function getShellArgs(shell: string): string {
+    return (shell === 'bash' || shell === 'sh') ? "--norc" : "";
+}
+
 export function runShellCommand(command: string, cwd: fs.PathLike, callback?: (process: child_process.ChildProcess) => any): Thenable<ShellOutput> {
     let options: child_process.ExecOptions = {
         cwd: cwd.toString(),
         maxBuffer: 1024 * 1024
     };
-    const shell = getExtensionConfiguration('shell');
-
-    const shell_args = (shell === 'bash' || shell === 'sh') ? "--norc" : "";
+    const shell = getShell();
+    const shell_args = getShellArgs(shell);
 
     return new Promise<ShellOutput>((resolve, reject) => {
         let shell_command = `${shell} ${shell_args} -c '${command}'`;
         console.log(`Running ${shell} command ${shell_command}`);
         let process = child_process.exec(shell_command, options, (error, stdout, stderr) => {
             const result = new ShellOutput(stdout, stderr, shell_command);
-            if (error) {
+            if (error !== null) {
                 result.error = error;
                 reject(result);
             } else {
@@ -44,31 +56,136 @@ export function runCommand(
     args: string[],
     environment: [string, string][],
     cwd: fs.PathLike,
-    callback?: (process: child_process.ChildProcess) => any)
-    : Thenable<ShellOutput> {
-    let environment_kv = {};
-    for (let v of environment) {
-        environment_kv[v['name']] = v['value'];
-    }
-    let options: child_process.ExecOptions = {
-        cwd: cwd.toString(),
-        maxBuffer: 1024 * 1024,
-        env: environment.length === 0 ? process.env : environment_kv
-    };
+    additional_env_vars?: [string, string][],
+    callback?: (process: child_process.ChildProcess) => any,
+    out?: (lines: string) => void,
+    error?: (lines: string) => void
+): Thenable<ShellOutput> {
     return new Promise<ShellOutput>((resolve, reject) => {
         let full_command = `${command} ${args.join(" ")}`;
+
+        if (cwd === undefined) {
+            let result = new ShellOutput("", "", full_command);
+            console.error("Cannot run command, cwd is undefined");
+            console.log("Cannot run command, cwd is undefined");
+            result.error = new Error("Cannot run command, cwd is undefined");
+
+            reject(result);
+        }
+
+        if (!fs.existsSync(cwd.toString())) {
+            let result = new ShellOutput("", "", full_command);
+            console.error(`Invalid working directory, working directory ${cwd} does not exist`);
+            console.log(`Invalid working directory, working directory ${cwd} does not exist`);
+            result.error = new Error(`Invalid working directory, working directory ${cwd} does not exist`);
+            reject(result);
+        }
+
+        let environment_kv = {};
+        if (environment.length !== 0) {
+            for (const v of environment) {
+                environment_kv[v['name']] = v['value'];
+            }
+        } else {
+            for (const key in process.env) {
+                environment_kv[key] = process.env[key];
+            }
+        }
+        if (additional_env_vars !== undefined) {
+            for (const v of additional_env_vars) {
+                environment_kv[v[0]] = v[1];
+            }
+        }
+
+        let options: child_process.SpawnOptions = {
+            cwd: cwd.toString(),
+            detached: false,
+            shell: false,
+            killSignal: "SIGTERM",
+            env: environment_kv
+        };
         console.log(`Running async command ${full_command}`);
-        let process = child_process.execFile(command, args, options, (error, stdout, stderr) => {
-            const result = new ShellOutput(stdout, stderr, full_command);
-            if (error) {
+        try {
+            let pid = child_process.spawn(command, args, options);
+            let stdout = "";
+            let stderr = "";
+            console.log(`Spawned async full command ${full_command} with pid ${pid.pid} in directory ${cwd.toString()}`);
+            pid.stdout.on('data', (data) => {
+                stdout += data;
+                if (out !== undefined) {
+                    out(data.toString());
+                }
+            });
+            pid.stderr.on('data', (data) => {
+                stderr += data;
+                if (error !== undefined) {
+                    error(data.toString());
+                }
+            });
+            pid.on('error', (error) => {
+                console.error(error);
+                if (environment.length === 0) {
+                    console.error(`Command ${full_command} cannot be executed with process environment in ${cwd}`);
+                    for (const key in environment_kv) {
+                        console.log(`- ${key}: ${environment_kv[key]}`);
+                    }
+                    console.error();
+                } else {
+                    console.error(`Command ${full_command} cannot be executed with custom environment in ${cwd}`);
+                }
+
+                const result = new ShellOutput("", "", full_command);
                 result.error = error;
                 reject(result);
-            } else {
-                resolve(result);
+            });
+
+            let exit_code = undefined;
+            let signal_code = undefined;
+            let result: ShellOutput = undefined;
+            let maybe_finalize = () => {
+                if ((exit_code === undefined && signal_code === undefined) || result === undefined) {
+                    // either exit or close has not yet been called
+                    return;
+                }
+                if (exit_code !== undefined) {
+                    if (exit_code !== 0) {
+                        reject(result);
+                    } else {
+                        resolve(result);
+                    }
+                } else {
+                    assert(signal_code !== undefined);
+                    result.error = new Error(`Killed by signal ${signal_code}`);
+                    reject(result);
+                }
+
+            };
+
+            pid.on('exit', (code) => {
+                if (code === null) {
+                    // killed by a signal
+                    console.log(`child process ${full_command} (pid  ${pid.pid}) killed with signal ${pid.signalCode}`);
+                    signal_code = pid.signalCode;
+                } else {
+                    exit_code = code;
+                    console.log(`child process ${full_command} (pid  ${pid.pid}) exited with exit code ${code}`);
+                }
+                maybe_finalize();
+            });
+            pid.on('close', () => {
+                console.log(`child process ${full_command} (pid  ${pid.pid}) closed`);
+                result = new ShellOutput(stdout, stderr, full_command);
+                maybe_finalize();
+            });
+            if (callback) {
+                callback(pid);
             }
-        });
-        if (callback) {
-            callback(process);
+        } catch (error) {
+            console.log(`child process ${full_command} failed with ${error}`);
+            const result = new ShellOutput("", "", full_command);
+            result.error = error;
+            console.error(error);
+            reject(result);
         }
     });
 }
