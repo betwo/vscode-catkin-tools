a8119c2b6302a5ba4f00614db6ac5cedc9aeae7d
Sebastian Buck <mail@betwo.eu>
2022-03-05 00:53:07 +0100

    Pin to v.1.64 for integration tests

--- src/test/integration/integration_test.ts
+++ src/test/integration/integration_test.ts
@@ -16,12 +16,13 @@ export async function run_integration_tests() {
 
         // Download VS Code, unzip it and run the integration test
         await runTests({
+            version: '1.64.0', // pinned because 1.65.0 crashes randomly. TODO: bump in the future
             extensionDevelopmentPath,
             extensionTestsPath,
             launchArgs: [
                 workspace_catkin_add_gtest,
                 "--disable-gpu", "--disable-gpu-compositing",
-                " --no-sandbox"
+                "--no-sandbox"
                 // This disables all extensions except the one being tested
                 // '--disable-extensions'
             ],
