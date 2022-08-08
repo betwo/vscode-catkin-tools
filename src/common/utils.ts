8d26934607c9e62502b0db0aaaf41a128a0b4e0f
Sebastian Buck <mail@betwo.eu>
2022-08-09 00:59:14 +0200

    Support undefined in wrapArray

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,7 +1,9 @@
 
 
 export function wrapArray<T>(value: T | T[]): T[] {
-    if (!Array.isArray(value)) {
+    if (value === undefined) {
+        return undefined;
+    } else if (!Array.isArray(value)) {
         return [value];
     } else {
         return value;
